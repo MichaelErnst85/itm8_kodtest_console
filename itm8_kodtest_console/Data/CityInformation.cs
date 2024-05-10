e6d013076cd4424fc03e276ef9e43a07da95ebf5
Micha <Micha@MICHAELS-DATOR>
2024-05-10 12:10:34 +0200

    Add project files.

---
+++ itm8_kodtest_console/Data/CityInformation.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace itm8_kodtest_console.Data
+{
+    [JsonObject]
+    public class CityInformation
+    {
+        [JsonProperty("city")]
+        public string City { get; set; }
+        [JsonProperty("longitude")]
+        public string Longitude { get; set; }
+        [JsonProperty("latitude")]
+        public string Latitude { get; set; }
+        [JsonProperty("country")]
+        public string Country { get; set; }
+        [JsonProperty("temperature")]
+        public double Temperature { get; set; }
+    }
+}
---
+++ itm8_kodtest_console/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using itm8_kodtest_console.Data;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+try
+{
+    using HttpClient client = new HttpClient();
+
+    HttpResponseMessage response = await client.GetAsync(@"http://localhost:5187/WeatherInformation");
+    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+    if (!response.IsSuccessStatusCode)
+    {
+        Console.WriteLine("Failed to call API");
+        return;
+    }
+
+    var content = await response.Content.ReadAsStringAsync();
+    Console.WriteLine("API call success!\n");
+
+    if (content is null)
+    {
+        Console.WriteLine("API returned null");
+    }
+
+    List<CityInformation>? cities = JsonConvert.DeserializeObject<List<CityInformation>>(content);
+
+    cities = cities.OrderBy(x => x.Country).ThenByDescending(y => y.Temperature).ToList();
+
+    foreach (var city in cities)
+    {
+        Console.WriteLine($"{city.City}, temperature is now {city.Temperature}°C");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Something went belly up!\n\n{ex.Message}");
+}
